be6754ca50faa7907a39fbbb82ed540d43f200c4
Yslan Ramos <yslan3000@gmail.com>
2021-05-01 00:28:33 -0300

    chore: add credo

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule EmbedValidator.MixProject do
   defp deps do
     [
       {:skooma, "~> 0.2.0"},
-      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
+      {:mix_test_watch, "~> 1.0", only: :test, runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
--- test/embed_validator_test.exs
+++ test/embed_validator_test.exs
@@ -1,4 +1,5 @@
 defmodule EmbedValidatorTest do
+  @moduledoc false
   use ExUnit.Case
   doctest EmbedValidator
 
@@ -9,30 +10,30 @@ defmodule EmbedValidatorTest do
   end
 
   test "should return the embed when valid title is provided" do
-    title_embed = %{ title: "valid_title" }
+    title_embed = %{title: "valid_title"}
     sut = EmbedValidator.valid?(title_embed)
     assert Map.equal?(sut, title_embed)
   end
 
   test "should trigger an error when invalid title is provided" do
-    {:error, _reason} = EmbedValidator.valid?(%{ title: 1 })
-    {:error, _reason} = EmbedValidator.valid?(%{ title: :atom })
-    {:error, _reason} = EmbedValidator.valid?(%{ title: %{} })
+    {:error, _reason} = EmbedValidator.valid?(%{title: 1})
+    {:error, _reason} = EmbedValidator.valid?(%{title: :atom})
+    {:error, _reason} = EmbedValidator.valid?(%{title: %{}})
   end
 
   test "should trigger an error when title is must great" do
     big_title = String.duplicate("invalid_title", 400)
-    {:error, _reason} = EmbedValidator.valid?(%{ title: big_title })
+    {:error, _reason} = EmbedValidator.valid?(%{title: big_title})
   end
 
   test "should return the embed when valid description is provided" do
-    valid_description = %{ description: "any_description" }
+    valid_description = %{description: "any_description"}
     sut = EmbedValidator.valid?(valid_description)
     assert Map.equal?(sut, valid_description)
   end
 
   test "should trigger an error when description is must great" do
     big_description = String.duplicate("invalid_description", 1000)
-    {:error, _reason} = EmbedValidator.valid?(%{ title: big_description })
+    {:error, _reason} = EmbedValidator.valid?(%{title: big_description})
   end
 end
