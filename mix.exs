36e5ee8c1526887454dabd9ac566ee2d35c0eec5
Yslan Ramos <yslan3000@gmail.com>
2021-05-05 01:57:11 -0300

    add dialyxir and ex_doc

--- mix.exs
+++ mix.exs
@@ -24,6 +24,9 @@ defmodule DiscordEmbedValidator.MixProject do
       {:skooma, "~> 0.2.0"},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:earmark, "~> 1.2", only: :dev},
+      {:ex_doc, "~> 0.19", only: :dev},
+      {:dialyxir, "~> 0.4", only: :dev},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
